be29b06cad677379e3ddbef73092ebcb844b6fb8
IsraelaZimru <slzimro@gmail.com>
2021-10-17 16:40:17 +0300

    git commit -m git commit -m

---
+++ server/app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+//connecting to Socket.io
+var sockIO = require('socket.io')();
+app.sockIO = sockIO;
+
+
+sockIO.on('connection', function (socket) {
+    console.log('A client connection occurred!');
+});
+
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
